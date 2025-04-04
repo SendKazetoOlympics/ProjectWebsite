f881ab18a73f56a0a381afded6625d1eb17dfea5
Kaze Wong <kazewong.physics@gmail.com>
2025-04-03 21:09:32 -0400

    Add coming soon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="flex justify-center items-center h-screen">
+    <h1
+	class="bg-linear-to-r from-indigo-400 to-blue-700 bg-clip-text text-transparent dark:from-blue-500 dark:to-cyan-300"
+	>Coming Soon</h1>
+
+</div>
+
